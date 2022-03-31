8efabf378ddac28ce843511c96d9736f03774a92
LastLightIt <timlastlight98@gmail.com>
2022-03-31 10:11:17 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+# from tkinter import *
+
+# root = Tk()
+
+
+
+
+
+# def Hello(event):
+#     print('Hello world!')
+
+# btn = Button(root, text='Click me', width=30, height=5, bg='white', fg='black')
+
+# btn.bind('<Button-1>', Hello)
+# btn.pack()
+# root.mainloop()
+
+
+# Классы
+
+
+class Dog():
+    """Простая модель собаки"""
+
+    def __init__(self, name, age):
+
+        """Инициализируем атрибуты имя и возраст"""
+        self.name = name
+        self.age = age
+        print('Собака создана')
+
+
+    def sit(self):
+        """Собака будет садиться по команде"""
+        print(self.name.title() + 'собака села')
+
+
+    def jump(self):
+        """Собака будет перекатываться по команде"""
+        print(self.name.title() + ' собака перекатилась')
+
+    
+my_dog = Dog('Tolpik', 2)
+my_dog_2 = Dog('Reks', 12)
+
+print(my_dog.age)
+print(my_dog_2.age)
+print(my_dog_2.name)
+
+my_dog.jump()
+my_dog_2.jump()
